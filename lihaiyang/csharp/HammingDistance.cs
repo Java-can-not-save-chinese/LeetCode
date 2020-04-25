00c0f16a8bebe09ee2717144a4e81b941209b968
Hyan Lee <hyan23lee@hotmail.com>
2020-04-25 16:56:12 +0800

    Added HammingDistance.cs

--- lihaiyang/csharp/HammingDistance.cs
+++ lihaiyang/csharp/HammingDistance.cs
@@ -1,10 +1,12 @@
-// NumberOf1Bits.cs
+// HammingDistance.cs
 // Author: hyan23
 // 2020.04.25
-// https://leetcode.com/explore/featured/card/top-interview-questions-easy/99/others/565/
+// https://leetcode.com/explore/featured/card/top-interview-questions-easy/99/others/762/
 
-// Runtime: 36 ms
-// Memory Usage: 14.7 MB
+// Runtime: 40 ms
+// Memory Usage: 14.6 MB
+
+using System;
 
 namespace csharp
 {
@@ -17,6 +19,7 @@ namespace csharp
 
         public void Test()
         {
+            Console.WriteLine(HammingDistance(12452, 4342235));
         }
 
         public int HammingWeight(uint n)
@@ -29,6 +32,11 @@ namespace csharp
                 n >>= 4;
             }
             return nb;
+        }
+
+        public int HammingDistance(int x, int y)
+        {
+            return HammingWeight((uint)(x ^ y));
         }
     }
 }
