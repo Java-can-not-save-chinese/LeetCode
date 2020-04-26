4c1b69c598b0f53f5772081b9895985eb16c6553
jethrozz <208499067@qq.com>
2020-04-26 22:16:31 +0800

    完成leetCode226、leetCode191、leetCode202、leetCode232

---
+++ xinchengzhu/week4/LeetCode_191.java
@@ -0,0 +1,41 @@
+package LeetCode.xinchengzhu.week4;
+
+/**
+ * 编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：00000000000000000000000000001011
+ * 输出：3
+ * 解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。
+ * 示例 2：
+ *
+ * 输入：00000000000000000000000010000000
+ * 输出：1
+ * 解释：输入的二进制串 00000000000000000000000010000000 中，共有一位为 '1'。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-1-bits
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode_191 {
+
+    // you need to treat n as an unsigned value
+    public static int hammingWeight(int n) {
+        int count=0;
+        String bin = Integer.toBinaryString(n);
+        for(char c : bin.toCharArray()){
+            if(c == '1'){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
