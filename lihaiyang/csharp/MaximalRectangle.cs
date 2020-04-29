ce3f264d0e4aa2abfce48fb550d71e18b6cac534
Hyan Lee <hyan23lee@hotmail.com>
2020-04-29 14:38:30 +0800

    Added FirstUniqueNumber.cs

---
+++ lihaiyang/csharp/MaximalRectangle.cs
@@ -0,0 +1,23 @@
+// MaximalRectangle.cs
+// Author: hyan23
+// 2020.04.29
+// https://leetcode.com/problems/maximal-rectangle/
+
+// 
+
+using System.Collections.Generic;
+
+namespace csharp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            new Program().Test();
+        }
+
+        public void Test()
+        {
+        }
+    }
+}
