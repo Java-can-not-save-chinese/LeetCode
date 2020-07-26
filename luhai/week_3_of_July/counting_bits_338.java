ce4921b276780b86f0bca5fa17aee96f48208d43
tigerLuHai <48075694+tigerLuHai@users.noreply.github.com>
2020-07-26 16:15:39 +0800

    七月第三周

---
+++ luhai/week_3_of_July/counting_bits_338.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] countBits(int num) {
+        int dp[] = new int[num + 1];
+        for(int i = 1; i <= num;i++){
+            dp[i] = dp[i & (i - 1)] + 1;
+        }
+        return dp;
+    }
+}
